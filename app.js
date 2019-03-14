6cb9b8a1b5ff8c9e2df671e75e12deebd893cc74
nkiyo <nkiyo@hoge.com>
2019-03-15 07:48:08 +0900

    up

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
-class Person{
+class Person {
   constructor(name) {
-    this.name =   name;
+    this.name = name;
   }
-
 }
 
-
+/** comment1 */
 const profile = {
-  name: 'soarflat', sex: 'male', location: 'Tokyo'
-
+  name: "soarflat",
+  sex: "male",
+  location: "Tokyo"
 };
 
-const hoge = ()=>{
-  console.log('hoge');
-}
-
-const fooBar = (foo, bar) =>{
-    console.log(foo);
-  console.log(bar)
+/**
+ * comment2
+ */
+const hoge = () => {
+  console.log("hoge");
 };
 
-fooBar(111, 
-    {
-        hoge: 'hoge!'
-    }
-)
+const fooBar = (foo, bar) => {
+  console.log(foo);
+  console.log(bar);
+  const a = [1, 2, 3];
+  let sum = 0;
+  for (const i in a) {
+    sum += a[i];
+  }
+};
+
+fooBar(111, {
+  hoge: "hoge!"
+});
